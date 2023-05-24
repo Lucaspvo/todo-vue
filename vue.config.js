2954ac0aaa2c66c62719307676f66b3864637e2b
Lucas Pazelo <lucaspazello@gmail.com>
2023-05-24 17:45:42 -0400

    Added sass pre-processor for css

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        implementation: require('sass'),
+        sassOptions: {
+          fiber: require('fibers'),
+          indentedSyntax: true
+        }
+      }
+    }
+  }
 })
